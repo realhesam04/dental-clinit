d4fc91eb98d6629f9b3427143d9b5362a5defa2b
realhesam <hesamehsanimoghaddam2004@gmail.com>
2026-08-11 10:27:53 +0430

    add DoctorAdmin to doctors/admin.py

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Doctor
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = (
+        'get_full_name',
+        'specialty',
+        'experience',
+        'is_active',
+        'created_at',
+    )
+
+    list_filter = (
+        'specialty',
+        'is_active',
+    )
+
+    search_fields = (
+        'user__first_name',
+        'user__last_name',
+        'user__username',
+        'user__phone',
+
+    )
+    ordering = ('-created_at',)
+
+    @admin.display(description='Doctor')
+    def get_full_name(self, obj):
+        return obj.user.get_full_name() or obj.user.username
+    
