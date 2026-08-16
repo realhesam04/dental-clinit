870340379dd5fdf43bd985411d75689cfbe57f2a
realhesam <hesamehsanimoghaddam2004@gmail.com>
2026-08-17 03:42:44 +0430

    add model for Patients app

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Patient(models.Model):
+    GENDER_CHOICE = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='patient_profile',
+    )
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    gender = models.CharField(
+        max_length=10,
+        choices=GENDER_CHOICE,
+        blank=True,
+    )
+    address = models.TextField(
+        blank=True,
+    )
+    medical_notes = models.TextField(
+        blank=True,
+        help_text= "General mdeical notes",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
+
