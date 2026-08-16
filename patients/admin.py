29d81f897f14d1d707099bab1f82b3f6e963120e
realhesam <hesamehsanimoghaddam2004@gmail.com>
2026-08-17 03:52:16 +0430

    add admin for Patient app

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Patient
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = [
+        'get_full_name',
+        'gender',
+        'date_of_birth',
+        'created_at',
+    ]
+    list_filter = (
+        'gender',
+    )
+    search_fields = (
+        'user__firstname',
+        'user__lastname',
+        'user__username',
+        'user__phone',
+    )
+    ordering = (
+        '-created_at',
+    )
+
+    @admin.display(description='Patient')
+    def get_full_name(self, obj):
+        return obj.user.get_full_name() or obj.user.username
+
+    
